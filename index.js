a4553eb6f3567e4561448923d8075c1da3b5b37e
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-09 05:35:46 +0100

    added comments

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ app.listen(port, ()=>{ console.log('Hello! I\'m listening on port:3000');
 app.use(express.static('public'));
 app.use(express.json({limit : '1mb'}));
 
+//receiving the data that the client sent to the api endpoint ('/api')
 app.post('/api', (request, response) =>{
+
+    //lets see the body of the content of the request
     console.log(request.body);
 
+    //preparing to return the receieved content back to the client just so they know you got 
+    //their message(talk about being polite)
     response.json({
         status : 'success',
         latitude : request.body.lat,
